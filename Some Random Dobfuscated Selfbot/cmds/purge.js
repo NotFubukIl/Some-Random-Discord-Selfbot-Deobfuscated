a48dca13eaa0676ffb9c936c79c10e31a98a6a5e
GayarraFrost <75570490+GayarraFrost@users.noreply.github.com>
2021-09-17 23:42:35 +0200

    Delete Some Random Dobfuscated Selfbot/cmds directory

--- Some Random Dobfuscated Selfbot/cmds/purge.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-module.exports.run = (client, message, args) => {
-    message.delete();
-    message.guild.pruneMembers(1).then(e =>
-        console.log("Matei" + e + "pessoas!")).catch(console.error);
-};
